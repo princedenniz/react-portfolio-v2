7f0b69d0076d24c8c412c8a4dfe13c1614096346
Prince Dennis <dennisprince815@gmail.com>
2023-12-06 01:02:32 +0100

    mostRecent

--- src/Projects/ProjectMenu.jsx
+++ src/Projects/ProjectMenu.jsx
@@ -19,9 +19,9 @@ export default class ProjectMenu extends Component {
   renderContent = (projects) => {
     return projects.map((project, index) => (
       <div key={index} className={`project-sub-container-${index + 1}`}>
-        <h3>{project.title}</h3>
+        <h3 className="project" >{project.title}</h3>
         <img src={project.image} alt={project.title} className="project-img" />
-        <div>{project.description}</div>
+        <div className="project">{project.description}</div>
         <div className="link-container">
           <a href={project.github} target="_blank" rel="noopener noreferrer">
             GITHUB
